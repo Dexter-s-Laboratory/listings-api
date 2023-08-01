32efb88fa3e75ea6c2d0a2a3cbdb5fbcba61793a
Patrick Nilan <nilan.patrick@gmail.com>
2023-08-01 08:18:59 -0700

    Added initial controller function stubs

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,2 +1,24 @@
-exports.example = require('./example.js');
-exports.rootJestTest = require('./rootJestTest.js')
+const model = require('../models');
+
+module.exports = {
+
+  getListings: (req, res) => {
+    res.end();
+  },
+
+  getListingById: (req, res) => {
+    res.end();
+  },
+
+  createListing: (req, res) => {
+    res.end();
+  },
+
+  updateListingDetails: (req, res) => {
+    res.end();
+  },
+
+  deleteListing: (req, res) => {
+    res.end();
+  }
+};
