f5b39c4f894694fe031b597ba29150ceb4e117db
ahusu <su.aust@gmail.com>
2023-08-02 11:57:34 -0700

    updated routes according to patrick's comments

--- server/routes.js
+++ server/routes.js
@@ -2,29 +2,29 @@ var controller = require('./controllers');
 var router = require('express').Router();
 
 // GET /api/listings?query=
-router.get('/api/lp', controller.getProducts);
+router.get('/lp', controller.getProducts);
 
 // GET /api/lp/:listing_id (product)
-router.get('/api/lp/:listing_id', controller.getListingById);
+router.get('/lp/:listing_id', controller.getListingById);
 
 // GET mylistings by user_id
-router.get('/api/lp/mylistings/:user_id', controller.getMyListings);
+router.get('/lp/mylistings/:user_id', controller.getMyListings);
 
 //GET listings by
-router.get('/api/lp/user/:user_id', controller.getListingByUserId);
+router.get('/lp/user/:user_id', controller.getListingByUserId);
 
 
 // POST /api/listings (authentication required)
-router.post('/api/lp', controller.createListing);
+router.post('/lp', controller.createListing);
 
 //POST /api/products
-router.post('/api/lp', controller.createProduct)
+router.post('/lp/products', controller.createProduct)
 
 // PUT /api/listings/:listing_id/:attribute (authentication required)
-router.put('/api/lp/:listing_id/details', controller.updateListingDetails);
+router.put('/lp/:listing_id/details', controller.updateListingDetails);
 
 // DELETE /api/listings/:listing_id (authentication required)
-router.delete('/api/lp/:listing_id', controller.deleteListing);
+router.delete('/lp/:listing_id', controller.deleteListing);
 
 module.exports = router;
 
