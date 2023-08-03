a75728e33c6b537063538d72d355d13f49831bab
Muzammil Waheed <muzammilwaheed101@gmail.com>
2023-08-02 20:27:35 -0700

    fix some routing issues

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,70 +1,78 @@
 const model = require("../models");
 
 module.exports = {
-  getProducts: (req, res) => {
+  getProducts: async (req, res) => {
     try {
-      let products = model.getProductsFromDB();
-      res.send(products);
+      let products = await model.getProductsFromDB();
+      res.status(200).send(products);
     } catch (err) {
       res.status(400).send(err);
     };
   },
 
-  getListingById: (req, res) => {
+  getListingById: async (req, res) => {
     try {
-      let id = req.query.listing_id;
-      res.send(model.getListingsByIdFromDB(id));
+      let id = req.params.listing_id;
+      let listing = await model.getListingsByIdFromDB(id);
+      res.status(200).send(listing);
     } catch (err) {
       res.status(400).send(err);
     };
   },
 
-  getListingByUserId: (req, res) => {
+  getListingByUserId: async (req, res) => {
     try {
-      let id = req.query.user_id;
-      res.send(model.getActiveListingsByUserIdFromDB(id));
+      let id = req.params.user_id;
+      let listing = await model.getActiveListingsByUserIdFromDB(id);
+      console.log(listing)
+      res.status(200).send(listing);
     } catch (err) {
       res.status(400).send(err);
     };
   },
 
-  getMyListings: (req,res) => {
+  getMyListings: async (req,res) => {
     try {
-      let id = req.query.user_id;
-      res.send(model.getMyListingsFromDB(id));
+      let id = req.params.user_id;
+      let listing = await model.getMyListingsFromDB(id);
+      res.status(200).send(listing);
     } catch (err) {
       res.status(400).send(err);
     };
   },
 
-  createListing: (req, res) => {
+  createListing: async (req, res) => {
     try {
-      res.status(201).send(model.createListingInDB(req.body));
+      await model.createListingInDB(req.body)
+      res.status(201).send('Listing has been created');
     } catch (err) {
       res.status(400).send(err);
     };
   },
-  createProduct: (req,res) => {
+  createProduct: async (req,res) => {
     try {
-      res.status(201).send(model.createProductInDB(req.body))
+      await model.createProductInDB(req.body)
+      res.status(201).send('Product has been created')
     } catch (err) {
       res.status(400).send(err);
     };
   },
 
-  updateListingDetails: (req, res) => {
+  updateListingDetails: async (req, res) => {
     let id = req.params.listing_id;
     try {
-      res.send(model.updateListingDetailsInDB(id, req.body));
+      await model.updateListingDetailsInDB(id, req.body)
+      res.send('Listing has been updated');
     } catch (err) {
       res.status(400).send(err);
     };
   },
 
-  deleteListing: (req, res) => {
+  deleteListing: async (req, res) => {
     let id = req.params.listing_id;
     try {
-      res.status(204).send(model.deleteListingFromDB(id, req.body));
+      await model.deleteListingFromDB(id, req.body);
+      res.status(204).send('listing has been deleted');
     } catch (err) {
       res.status(400).send(err);
     };
